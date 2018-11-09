50e8e4755830551ac6e12698f2e6527b330d5a25
michelet <michelet@usp.br>
2018-11-09 16:25:18 -0200

    Tirando arquivos desnecessários

--- lista/models/item.py
+++
@@ -1,8 +0,0 @@
-'''
-from datetime import datetime
-class Item(object):
-    def __init__(self, _id, nome):
-        self.id = _id
-        self.nome = nome
-        self.dataCriacao = datetime.now()
-'''
